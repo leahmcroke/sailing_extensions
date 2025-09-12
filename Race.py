2fe80dca03668d696dc818b2aa03d367b038efa1
leahmcroke <145214077+leahmcroke@users.noreply.github.com>
2025-09-12 17:38:52 -0400

    race class

--- Race.py
+++ Race.py
@@ -1,6 +1,24 @@
+import requests
+import boat_secrets
 
 class Race:
 
-    placeholder
+    def __init__(self, race_id):
+        self.race_id = race_id
+
+        response = requests.get('https://8bitbyte.ca/sailnavsim/api/raceinfo.php', {'id': self.race_id})
+        race_data = response.json()
+
+        self.finish_min_lat = race_data['finishMinLat']
+        self.finish_min_lon = race_data['finishMinLon']
+        self.finish_max_lat = race_data['finishMaxLat']
+        self.finish_max_lon = race_data['finishMaxLon']
+        
+        self.finish_point = self.finish_point()
+
+    def finish_point(self):
+        finish_point_lat = ((self.finish_max_lat-self.finish_min_lat)/2)+self.finish_min_lat
+        finish_point_lon = ((self.finish_max_lon-self.finish_min_lon)/2)+self.finish_min_lon
+        return (finish_point_lat, finish_point_lon)
 
 
--- tester.py
+++ tester.py
@@ -1,7 +1,12 @@
 
 import Boat
+import Race
 import boat_secrets
 
 my_boat = Boat.Boat(boat_secrets.boat_key)
 
 print(my_boat.wind_speed)
+
+my_race = Race.Race(boat_secrets.race_id)
+
+print(my_race.finish_point)
